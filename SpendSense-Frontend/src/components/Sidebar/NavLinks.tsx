1d9f7d5d6ca1a999ed85c4bfcfaad4f19755b5f5
Aman Bhatt <amanbhatt0910@gmail.com>
2024-11-27 12:16:56 +0530

    routing changes

--- SpendSense-Frontend/src/components/Sidebar/NavLinks.tsx
+++ SpendSense-Frontend/src/components/Sidebar/NavLinks.tsx
@@ -11,6 +11,7 @@ import {
   ClipboardDocumentCheckIcon,
   BellAlertIcon,
 } from "@heroicons/react/24/outline";
+import {useNavigate} from "react-router-dom"
 
 type HeroIcon = React.ComponentType<React.SVGProps<SVGSVGElement> & { title?: string; titleId?: string }>;
 
@@ -26,6 +27,12 @@ const iconMap: { [key: string]: HeroIcon } = {
   BellAlertIcon,
 };
 
+const nav = useNavigate()
+
+function pushLink(link: string){
+  nav(link)
+}
+
 const NavLinks: React.FC = () => {
   return (
     <nav className="flex-1 p-4 space-y-3">
@@ -34,7 +41,7 @@ const NavLinks: React.FC = () => {
         return (
           <a
             key={item.id}
-            href={item.href}
+            onClick={() => pushLink(item.href)}
             className="flex items-center space-x-4 p-3 rounded-lg bg-primary text-white hover:bg-white hover:text-primary transition-all duration-300 ease-in-out"
           >
             {IconComponent && (
